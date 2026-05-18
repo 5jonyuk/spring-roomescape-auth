2e52e70ff6967668ff980dfe4e8f2534241cd0ac
ohjonghyuk <ohjonghyuk0717@naver.com>
2026-05-18 21:57:29 +0900

    feat: 방탈출 사용자 예약 코드 마이그레이션

---
+++ src/test/java/roomescape/MissionStepTest.java
@@ -0,0 +1,27 @@
+package roomescape;
+
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+import roomescape.config.TestTimeConfig;
+
+import static org.hamcrest.Matchers.is;
+
+@ActiveProfiles("test")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@Sql(scripts = {"/truncate.sql", "/test-data.sql"}, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+@Import(TestTimeConfig.class)
+public class MissionStepTest {
+
+    @Test
+    void 예약_조회() {
+        RestAssured.given().log().all()
+                .when().get("/reservations")
+                .then().log().all()
+                .statusCode(200)
+                .body("data.size()", is(4));
+    }
+}
