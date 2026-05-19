ef6e2bd532a354819d0eda5ed99a926868d93182
ohjonghyuk <ohjonghyuk0717@naver.com>
2026-05-19 17:23:40 +0900

    feat: argumentResolver, interceptor 적용

--- src/main/java/roomescape/config/WebConfig.java
+++ src/main/java/roomescape/config/WebConfig.java
@@ -1,11 +1,24 @@
 package roomescape.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import roomescape.common.interceptor.AdminInterceptor;
+import roomescape.common.interceptor.AuthenticationInterceptor;
+import roomescape.common.argumentresolver.LoginMemberArgumentResolver;
+
+import java.util.List;
 
 @Configuration
+@RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
+
+    private final LoginMemberArgumentResolver loginMemberArgumentResolver;
+    private final AuthenticationInterceptor authenticationInterceptor;
+    private final AdminInterceptor adminInterceptor;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
@@ -16,5 +29,28 @@ public class WebConfig implements WebMvcConfigurer {
                 )
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(loginMemberArgumentResolver);
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(authenticationInterceptor)
+                .addPathPatterns("/reservations/**")
+                .excludePathPatterns("/login");
+        registry.addInterceptor(adminInterceptor)
+                .addPathPatterns(
+                        "/times/**",
+                        "/themes/**",
+                        "/schedules/**"
+                )
+                .excludePathPatterns(
+                        "/themes",
+                        "/themes/popular",
+                        "/times/availability"
+                );
     }
 }
